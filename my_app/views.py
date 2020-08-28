fbc9c0f6d3d6598c91f9281b575fbc51ef4ac4e9
akshay-1993 <chordiaakshay1993@gmail.com>
2020-08-28 02:02:36 +0100

    Add craigslist search functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,17 +1,53 @@
-
 import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
 
 # Create your views here.
+BASE_CRAIGLIST_URL = "https://losangeles.craigslist.org/search/?query={}"
+BASE_IMAGE_URL = "https://images.craigslist.org/{}_300x300.jpg"
+
 
 def home(request):
-    return render(request,template_name='base.html')
+    return render(request, template_name='base.html')
+
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features="html.parser")
+    # post_titles = soup.find_all('a',{'class':'result-title'})
+    # print(post_titles)
+    # print(data)
+    # print(search)
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+           post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'NA'
+
+        if post.find(class_ = 'result-image').get('data-ids'):
+            post_image = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url  = BASE_IMAGE_URL.format(post_image)
+            print(post_image_url)
+
+        else:
+            post_image_url = "https://craigslist.org/images/peace.jpg"
+
+        final_postings.append((post_title, post_url, post_price,post_image_url))
+    # print(final_postings)
     stuff_for_frontend = {
-        'search':search,
+        'search': search,
+        'final_postings':final_postings,
     }
-    return render(request,'my_app/new_search.html', stuff_for_frontend)
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
